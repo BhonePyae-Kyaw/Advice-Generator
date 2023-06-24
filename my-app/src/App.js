d9de1dea9360ed9b4acf3c2090db9aa79b2fe7fc
BHONE PYAE KYAW <137211983+BhonePyae-Kyaw@users.noreply.github.com>
2023-06-24 17:20:14 +0700

    Data fetched and displayed with react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [result, setResult] = useState();
+
+  useEffect(() => {
+    getData();
+  },[]);
+
+  const getData = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then(res => res.json())
+      .then(
+        (resultData) => {
+          console.log(resultData)
+          setResult(resultData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+  };
+
+  const nextAdvice = () => {
+    getData();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
+        {result && 
+        <div>
+          <h1>Advice # {result.slip.id}</h1>
+          <h2>{result.slip.advice}</h2>
+        </div>
+        }
+        <button onClick={nextAdvice}>Next Advice</button>
+
     </div>
   );
 }
